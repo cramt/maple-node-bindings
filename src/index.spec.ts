267f217a67e107e0681a196408a38ac42bbe54a0
mads østermark <madscramt@gmail.com>
2018-11-05 09:55:52 +0100

    stuff

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,9 @@
-import { MapleEngine } from "./main";
+import { MapleEngine } from ".";
 import { expect } from 'chai';
 import 'mocha';
 
+let maple = new MapleEngine("C:\\Program Files (x86)\\Maple 2018\\bin.win\\cmaple.exe")
 describe("initialize main", () => {
-    let maple = new MapleEngine("C:\\Program Files (x86)\\Maple 2018\\bin.win\\cmaple.exe")
     it("should calculate the function", () => {
         maple.eval("D(x->x^sin(x))").then(x => maple.evalf(x + "(32)").then(x => {
             expect(x).to.equal("19.66248767")
