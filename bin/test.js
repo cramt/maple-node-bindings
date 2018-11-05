b3c9f4ee539ca64e8a9f48335fd7a31ffe8df160
mads østermark <madscramt@gmail.com>
2018-11-05 09:15:36 +0100

    ye

--- bin/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./main");
-let a = new main_1.MapleEngine("C:\\Program Files (x86)\\Maple 2018\\bin.win\\cmaple.exe");
-a.eval("D(x->x^sin(x))").then(x => a.evalf(x + "(32)").then(console.log)).catch(console.error);
